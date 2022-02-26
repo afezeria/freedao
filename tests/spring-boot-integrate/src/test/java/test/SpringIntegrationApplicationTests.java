f85b9a07f3c8004eceb190e0f372aa97f361c74b
afezeria <zodal@outlook.com>
2022-02-26 17:03:49 +0800

    delete obsolete code

--- tests/spring-boot-integrate/src/test/java/test/SpringIntegrationApplicationTests.java
+++ tests/spring-boot-integrate/src/test/java/test/SpringIntegrationApplicationTests.java
@@ -1,18 +1,13 @@
 package test;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
+ *
  */
 @SpringBootTest
 public class SpringIntegrationApplicationTests {
-    @Autowired
-    JavaDao dao;
-
-    @Test
+    //    @Test
     public void contextLoads() {
-        System.out.println(dao.abc(1L, "y", 'c', null, null));
     }
 }
