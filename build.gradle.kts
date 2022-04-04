5068f2e7e3b8d376500ceb7693cb58a0d8ade46d
afezeria <zodal@outlook.com>
2022-04-04 22:59:44 +0800

    upgrade kotlin version to 1.6.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("org.springframework.boot") version "2.6.2" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
     kotlin("plugin.spring") version "1.6.10" apply false
-    kotlin("jvm") version "1.6.10" apply false
-    kotlin("kapt") version "1.6.10" apply false
+    kotlin("jvm") version "1.6.20" apply false
+    kotlin("kapt") version "1.6.20" apply false
     id("com.bnorm.power.kotlin-power-assert") version "0.11.0" apply false
 }
 
--- function-list.md
+++
@@ -1,9 +0,0 @@
-# 功能清单
-
-### 方法定义
-
-* [x] 标准CRUD方法
-* [x] Mybatis风格
-* [x] SpringDataJpa风格
-* [ ] 注解风格
-
