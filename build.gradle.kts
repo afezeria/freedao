0d18b00276b9a035a08a936675d95db503584ad7
afezeria <zodal@outlook.com>
2022-04-28 19:23:10 +0800

    chore: update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,14 @@ plugins {
     id("com.bnorm.power.kotlin-power-assert") version "0.11.0" apply false
 }
 
+val ossrhUsername: String by project
+val ossrhPassword: String by project
+
 subprojects {
+
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+    apply(plugin = "signing")
 
     repositories {
         mavenCentral()
@@ -17,6 +24,77 @@ subprojects {
     }
 
     group = "io.github.afezeria.freedao"
-    version = "0.0.1"
+    version = "0.1.0"
 
+    if (project.name.startsWith("test")) {
+        tasks.withType<Jar> {
+            enabled = false
+        }
+    } else {
+        val sourcesJar by tasks.creating(Jar::class) {
+            archiveClassifier.set("sources")
+            from(project.the<SourceSetContainer>()["main"].allSource)
+        }
+
+        val javadocJar by tasks.creating(Jar::class) {
+            archiveClassifier.set("javadoc")
+            from(tasks.withType<Javadoc>())
+        }
+
+        project.artifacts {
+            add("archives", sourcesJar)
+            add("archives", javadocJar)
+        }
+
+        configure<SigningExtension>() {
+            sign(configurations["archives"])
+        }
+
+        configure<PublishingExtension>() {
+            repositories {
+                maven {
+                    if (project.version.toString().endsWith("-SNAPSHOT")) {
+                        setUrl("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                    } else {
+                        setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                    }
+                    credentials {
+                        username = ossrhUsername
+                        password = ossrhPassword
+                    }
+                }
+            }
+
+            publications {
+                register<MavenPublication>("mavenJava") {
+                    from(components["java"])
+                    artifact(sourcesJar)
+
+                    pom {
+                        name.set("${project.group}:${project.name}")
+                        description.set("A dao library based on apt.")
+                        url.set("https://github.com/afezeria/freedao")
+                        licenses {
+                            license {
+                                name.set("The Apache Software License, Version 2.0")
+                                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                            }
+                        }
+                        scm {
+                            url.set("https://github.com/afezeria/freedao")
+                            connection.set("scm:git:https://github.com/afezeria/freedao.git")
+                            developerConnection.set("scm:git:ssh://git@github.com/afezeria/freedao.git")
+                        }
+                        developers {
+                            developer {
+                                id.set("afezeria")
+                                name.set("afezeria")
+                                email.set("zodal@outlook.com")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
